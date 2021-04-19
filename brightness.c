427b92ec4c8a2f911c6e58ff61e8aae2211b2442
alfille <paul.alfille@gmail.com>
2021-04-18 20:54:56 -0400

    Update brightness to find more generic control files

--- brightness.c
+++ brightness.c
@@ -10,18 +10,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <dirent.h>
 
-#define BackDir "/sys/class/backlight/intel_backlight/"
-#define KeyDir "/sys/class/leds/chromeos::kbd_backlight/"
+#define BackDir "/sys/class/backlight/"
+#define KeyDir "/sys/class/leds/"
 
 #define ControlCur "brightness"
 #define ControlMax "max_brightness"
+#define ControlType "type"
 
 char * progname = "program" ; // should be redirected
 
 void help( void )
 {
-    fprintf( stderr, "%s -- set the screen or keyboard brightness level for the Google Pixel Chromebook (2013)\n" , progname ) ;
+    fprintf( stderr, "%s -- set the screen or keyboard brightness level for this laptop\n" , progname ) ;
     fprintf( stderr, "\n") ;
     fprintf( stderr, "Writes to /sys/class -- needs root privileges\n" ) ;
     fprintf( stderr, "by Paul H Alfille 2021\n") ;
@@ -37,113 +40,191 @@ void help( void )
     fprintf( stderr, "\t%s -h -- this help\n", progname ) ;
 }
 
+long value( char * s )
+{
+    char * endptr ;
+    long b = strtol( s, &endptr, 10 ) ;
+
+    if ( (s == endptr) || (b<0.) || (b>100.) ) {
+        help() ;
+        fprintf( stderr, "\nInvalid numeric argument: %s\n", s );
+        exit(1) ;
+    }
+    return b ;
+}
+    
+
+int file_exists( char * dir, char * name )
+{
+    // return 0 if exists
+    struct stat sb ;
+    char Path[PATH_MAX] ;
+    
+    strcpy( Path, dir ) ;
+    strcat( Path, name ) ;
+
+    if ( stat( Path, &sb )==0 && S_ISREG(sb.st_mode) ) {
+        return 0 ;
+    }
+    return 1 ;
+}
+
+char * next_dir( DIR * dir )
+{
+    struct dirent * de ;
+
+    do {
+        de = readdir( dir ) ;
+        if ( de == NULL ) {
+            return NULL ;
+        }
+        if ( de->d_type == DT_DIR ) {
+            return de->d_name ;
+        }
+    } while (1) ;
+}
+
 FILE * file_open( char * dir, char * name, char * mode )
 {
-	char Path[1024] ;
-	FILE * f;
-	
-	strcpy( Path, dir ) ;
-	strcat( Path, name ) ;
+    char Path[PATH_MAX] ;
+    FILE * f;
+    
+    strcpy( Path, dir ) ;
+    strcat( Path, name ) ;
     f = fopen( Path, mode ) ;
     if ( f == NULL ) {
-	fprintf( stderr, "Cannot open %s: ", Path ) ;
-	perror( NULL ) ;
-	exit(1) ;
+        fprintf( stderr, "Cannot open %s: ", Path ) ;
+        perror( NULL ) ;
+        exit(1) ;
     }
     return f ;
 }
 
-long value_read( char * dir, char * name )
+void string_read( char * dir, char * name, char * buffer )
 {
     FILE * file = file_open( dir, name, "r" ) ;
-    char buffer[64] ;
     char * str = fgets( buffer, sizeof(buffer), file ) ;
 
     fclose( file ) ;
     if ( str == buffer ) {
-		return strtol( buffer, NULL, 10 ) ;
+        return ;
     }
     perror("Trouble reading file" ) ;
     exit(1) ;
 }
+
+long value_read( char * dir, char * name )
+{
+    char buffer[64] ;
+    string_read( dir, name, buffer ) ;
+    return value(buffer) ;
+}
     
 int main( int argc, char **argv )
 {
-
-    progname = argv[0] ; // point to invoked program name
+    char FullPath[ PATH_MAX ] ;
     
     // Arguments
     int c ;
-	char * endptr ;
-	long bright = -1. ;
-	long max_bright ;
-	char * Dir = BackDir ;
-	
+    long bright = -1. ;
+    long max_bright ;
+    char * Dir = BackDir ;
+
+    progname = argv[0] ; // point to invoked program name
+    
     while ( (c = getopt( argc, argv, ":b:k:h" )) != -1 ) {
-		switch ( c ) {
-			case 'h':
-				help() ;
-				return 0 ;
-			case 'b':
-				Dir = BackDir ;
-				bright = strtol( optarg, &endptr, 10 ) ;
-				if ( (optarg == endptr) || (bright<0.) || (bright > 100.) ) {
-					help() ;
-					fprintf( stderr, "\nInvalid numeric argument: %s\n", optarg );
-					exit(1) ;
-				}
-				break ;
-			case 'k':
-				Dir = KeyDir ;
-				bright = strtol( optarg, &endptr, 10 ) ;
-				if ( (optarg == endptr) || (bright<0.) || (bright > 100.) ) {
-					help() ;
-					fprintf( stderr, "\nInvalid numeric argument: %s\n", optarg );
-					exit(1) ;
-				}
-				break ;
-			case ':':
-				switch(optopt) {
-					case 'b':
-						Dir = BackDir ;
-						break ;
-					case 'k':
-						Dir = KeyDir ;
-						break ;
-					default:
-						help() ;
-						exit(1);
-				}
-				break ;
-			default:
-				help() ;
-				return 0 ;
-			break ;
-		}
+        switch ( c ) {
+            case 'h':
+                help() ;
+                return 0 ;
+            case 'b':
+                Dir = BackDir ;
+                bright = value( optarg ) ;
+                break ;
+            case 'k':
+                Dir = KeyDir ;
+                bright = value( optarg ) ;
+                break ;
+            case ':':
+                switch(optopt) {
+                    case 'b':
+                        Dir = BackDir ;
+                        break ;
+                    case 'k':
+                        Dir = KeyDir ;
+                        break ;
+                    default:
+                        help() ;
+                        exit(1);
+                }
+                break ;
+            default:
+                help() ;
+                return 0 ;
+            break ;
+        }
     }
 
     if ( argv[optind] != NULL ) {
-		bright = strtol( argv[optind], &endptr, 10 ) ;
-		if ( (argv[optind] == endptr) || (bright<0.) || (bright>100.) ) {
-			help() ;
-			fprintf( stderr, "\nInvalid numeric argument: %s\n", argv[optind] );
-			exit(1) ;
-		}
-	}
+        bright = value( argv[optind] ) ;
+    }
 
-	// max
-	max_bright = value_read( Dir, ControlMax ) ;
-	
-	if ( bright < 0. ) {
-		long cur = value_read( Dir, ControlCur ) ;
-		printf( "%3.0f\n",100.*cur/max_bright ) ;
-		return 0 ;
-	}
+    DIR * pdir = opendir( Dir ) ;
+    if ( pdir == NULL ) {
+        fprintf( stderr, "Cannot open directory %s\n", Dir ) ;
+        help() ;
+        exit(1) ;
+    }
+
+    char * trydir ;
+    FullPath[0] = '\0' ;
+    
+    // loop through directories
+    while ( (trydir = next_dir( pdir )) != NULL ) {
+        char trypath[PATH_MAX] ;
+        if ( Dir==KeyDir && strstr( trydir, "lock" ) ) {
+            continue ;
+        }
+            
+        strcpy( trypath, Dir ) ; // has terminating /
+        strcat( trypath, trydir ) ;
+        strcat( trypath, "/" ) ;
+        if ( file_exists( trypath, ControlMax ) != 0 ) {
+            continue ;
+        }
+        if ( file_exists( trypath, ControlCur ) != 0 ) {
+            continue ;
+        }
+        if ( file_exists( trypath, ControlType) == 0 ) {
+            char buffer[64] ;
+            string_read( trypath, ControlType, buffer ) ;
+            if ( strstr( buffer, "raw" ) ) {
+                strcpy( FullPath, trypath ) ;
+                break ;
+            }
+        }
+        strcpy( FullPath, trypath ) ;                
+    }
+
+    if ( FullPath[0] == '\0' ) {
+        fprintf( stderr, "Not appropriate directories found in %s\n", Dir ) ;
+        help();
+        exit(1) ;
+    }
+    
+    // max
+    max_bright = value_read( FullPath, ControlMax ) ;
+    
+    if ( bright < 0. ) {
+        long cur = value_read( FullPath, ControlCur ) ;
+        printf( "%3.0f\n",100.*cur/max_bright ) ;
+        return 0 ;
+    }
     
     // write brightness to control file
-    FILE * sys = file_open( Dir, ControlCur, "a" ) ;
+    FILE * sys = file_open( FullPath, ControlCur, "a" ) ;
     if ( fprintf( sys, "%ld\n", (long int) (bright*max_bright*.01) ) < 1 ) {
-		perror("Trouble writing to file" ) ;
+        perror("Trouble writing to file" ) ;
     }
     fclose( sys ) ;
     return 0 ;
